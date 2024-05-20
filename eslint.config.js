d177594b794c60f3945a963d306858828c8a8041
Jack Parsons <jackparse14@hotmail.co.uk>
2024-05-20 22:04:12 +0100

    Changed to node from browser

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,10 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
   pluginJs.configs.recommended,
-  { env: { node: true } },
 ];
