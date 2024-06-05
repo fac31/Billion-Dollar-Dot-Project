ba044f5a6e20e6f6e587dad7008fee8b92b12ac1
Jack Parsons <jackparse14@hotmail.co.uk>
2024-06-05 13:44:48 +0100

    Renamed public folder to docs for github pages

--- docs/js/intro.js
+++ docs/js/intro.js
@@ -6,25 +6,7 @@ export function intro() {
   const arrowElement = document.getElementById("fade-elements");
 
   setTimeout(() => fadeIn(arrowElement, 10, 1), 4000);
-  startIntro();
-  // setTimeout(3000);
-
-  // let maxMarginTop = -1000;
-  // for (const circle of circles.children) {
-  //   setTimeout(() => {
-  //     let timer = setInterval(function () {
-  //       if (circleMargin > maxMarginTop) {
-  //         circleMargin = circleMargin + 1;
-  //         circle.style.marginTop = circleMargin + "px";
-  //         console.log("move circle");
-  //       } else {
-  //         clearInterval(timer);
-  //       }
-  //     }, 10);
-  //   }, 4000);
-  // }
-}
-function startIntro() {
+  // startIntro();
   let maxMargin = -85;
   for (const circle of circles.children) {
     setTimeout(() => fadeIn(circle, 20, 0.5), 2000);
@@ -36,7 +18,6 @@ function startIntro() {
           circle.style.marginTop = circleMargin + "px";
         } else {
           clearInterval(timer);
-          setTimeout(() => fadeOut(circle, 20), 1000); // The console is saying fadeout is not defined - needs fixing
         }
       }, 10);
     }, 2000);
